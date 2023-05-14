80be09c1dfe770d2ddffe3c7f1b6cb73282ed50c
VoThiKimTrang06101997 <vothikimtrang0610@gmail.com>
2023-05-14 21:05:51 +0700

    config environment

---
+++ src/main/java/com/trang/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.trang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = { "/", "trang-chu" }, method = RequestMethod.GET)
+	public String homepage() {
+		return "user/index";
+	}
+}
